2ca7208bece8627a0cf58189d01b112a886b1594
hosseinfaraji8079 <h.faraji8079@gmail.com>
2024-12-24 13:37:50 +0330

    fixed jwt token generator

--- Registry.API/Services/UserService.cs
+++ Registry.API/Services/UserService.cs
@@ -1,19 +1,24 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Registry.API.Helpers;
 using Registry.API.Models;
 using Registry.API.Repositories.Interfaces;
 using Registry.API.ViewModel;
 
 namespace Registry.API.Services;
 
-public class UserService(IAsyncRepository<User> userRepository,IMapper mapper) : IUserService
+public class UserService(IAsyncRepository<User> userRepository, IMapper mapper) : IUserService
 {
-    public async Task<string> RegisterUserAsync(AddUserDto user)
+    public async Task<string> GetTokenUserAsync(AddUserDto user)
     {
         var mainUser = mapper.Map<User>(user);
-        await userRepository.AddAsync(mainUser);
-        
-        //generate token and send
-        
-        return "";
+
+        var existUser = await userRepository.GetQueryableAsync().SingleOrDefaultAsync(x => x.ChatId == user.ChatId);
+
+        return existUser switch
+        {
+            null => JwtHelper.GenerateToken(await userRepository.AddAsync(mainUser)),
+            _ => JwtHelper.GenerateToken(existUser)
+        };
     }
 }
--- Registry.API/ViewModel/AddUserDto.cs
+++ Registry.API/ViewModel/AddUserDto.cs
@@ -33,19 +33,4 @@ public class AddUserDto
     [Display(Name = "نام کاربری")]
     [MaxLength(50, ErrorMessage = "{0} نمی‌تواند بیشتر از {1} کاراکتر باشد.")]
     public string? UserName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the password of the user.
-    /// </summary>
-    [Display(Name = "رمز عبور")]
-    [Required(ErrorMessage = "رمز عبور اجباری است.")]
-    [MinLength(6, ErrorMessage = "{0} باید حداقل {1} کاراکتر باشد.")]
-    [MaxLength(100, ErrorMessage = "{0} نمی‌تواند بیشتر از {1} کاراکتر باشد.")]
-    public string? Password { get; set; }
-
-    /// <summary>
-    /// Gets or sets the role IDs associated with the user.
-    /// </summary>
-    [Display(Name = "نقش‌ها")]
-    public ICollection<long>? RoleIds { get; set; }
 }
